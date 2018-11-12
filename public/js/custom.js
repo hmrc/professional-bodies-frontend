250f841f87ff4ee06d6a6b6ac69f8ebafa99ae36
Ed Jannoo <ed.jannoo@devability.co.uk>
2018-11-12 10:47:08 +0000

    Add other letters of the alphabet

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-      $("#myInput").on("keyup", function() {
+      $("#search").on("keyup", function() {
         var value = $(this).val().toLowerCase();
         $(".organisation").filter(function() {
           $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
