b4fbd4ecafa2a6819e6539cfed32a410a9969779
Raysonviswas <rayson.viswas@digital.hmrc.gov.uk>
2018-11-13 16:57:39 +0000

    added function in home page to pull from a list and also added ability to put li elements in ul tags

--- app/uk/gov/hmrc/professionalbodiesfrontend/controllers/ProfessionalBodies.scala
+++ app/uk/gov/hmrc/professionalbodiesfrontend/controllers/ProfessionalBodies.scala
@@ -33,12 +33,39 @@ class ProfessionalBodies @Inject()(val messagesApi: MessagesApi, implicit val ap
 
     val organisations = Seq(
       "AABC Register Ltd (Architects accredited in building conservation),from year 2016 to 2017",
-      "Academic and Research Surgery Society of",
+        "Academic and Research Surgery Society of",
+        "Academic Gaming and Simulation in Education and Training Society for",
+    "Academic Primary Care Society for",
+    "Access Consultants National Register of",
+    "Accident and Emergency Medicine British Association for",
+    "Accountancy Association of Lecturers in",
+    "Accountants and Auditors British Association of",
+      "Accountants Association of International",
+      "Accountants Natal Society of",
+      "Accountants South African Institute of Professional (SAIPA)",
+    "Accountants Transvaal Society of",
+      "Accounting Association American",
+    "Accounting Association British (Standing Committee for British Accounting Association)",
+    "Accounting Association European",
+    "Accounting Conference of Professors of",
+    "Accounting Technicians Association of",
+      "Accounting Technicians in Ireland Institute of",
+      "Acoustical Society British",
+    "Acoustical Society of America",
+      "Acoustics Institute of",
+    "Acquisition of Latin American Library Materials Seminar on the (SALALM)",
+    "Actuarial Profession The (name for the Institute and Faculty of Actuaries)",
+    "Back Pain Research Society for",
+      "Bacteriologists Society of American (J - Applied Microbiology)",
+    "Bakers Institute of British",
+      "Radiation Biology European Society for",
       "Zoological Society of London (J - Proceedings)",
-      "Zoological Society of Scotland Royal"
+    "Zoological Society of Scotland Royal"
     )
 
     Future.successful(Ok(views.html.home(organisations)))
+
+
   }
 
 }
