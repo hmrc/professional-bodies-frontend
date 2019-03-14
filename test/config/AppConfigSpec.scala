7bd2d5744ff2e1495469524879a4773a7ddd6d6a
Christopher Townson <461513+iteratoruk@users.noreply.github.com>
2019-03-14 13:39:08 +0000

    Add unit test for AppConfig

---
+++ test/config/AppConfigSpec.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package config
+
+import uk.gov.hmrc.test.unigration.UnigrationSpec
+
+class AppConfigSpec extends UnigrationSpec {
+
+  "app config" should {
+
+    "have assets prefix" in {
+      appConfig.assetsPrefix must be("http://localhost:9032/assets/3.2.4")
+    }
+
+    "have analytics token" in {
+      appConfig.analyticsToken must be("N/A")
+    }
+
+    "have analytics host" in {
+      appConfig.analyticsHost must be("auto")
+    }
+
+    "have report a problem partial URL" in {
+      appConfig.reportAProblemPartialUrl must be("http://localhost:9250/contact/problem_reports_ajax?service=MyService")
+    }
+
+    "have report a problem non-JS URL" in {
+      appConfig.reportAProblemNonJSUrl must be("http://localhost:9250/contact/problem_reports_nonjs?service=MyService")
+    }
+
+    "have professional bodies API URL" in {
+      appConfig.professionalBodies must be("http://localhost:7401")
+    }
+
+  }
+
+}
